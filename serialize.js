d6edb13c671c8c8d603cb57200c886528643f499
Chris Johnson <tenorviol@yahoo.com>
2011-03-10 11:13:22 -0800

    a new beginning

---
+++ serialize.js
@@ -0,0 +1,39 @@
+
+function serialize(foo) {
+	var results = [],
+		count = 0,
+		responses = 0;
+	
+	// call all functions at once, sorting out the callback responses as they come
+	for (var i in arguments) {
+		count++;
+		arguments[i](function(sequence, data) {
+			console.log('callback: '+sequence);
+			results[sequence] = data;
+			responses++;
+			if (responses == count) {
+				console.log(results);
+			}
+		}, i);
+	}
+	
+	// TODO: one timeout to rule them all (aka cut them off)
+}
+
+// TODO: formalize into a test
+serialize(
+	function(callback, sequence) {
+		console.log('called: '+sequence);
+		setTimeout(function() {
+			callback(sequence, 'foo');
+		}, 20);
+	},
+	function(callback, sequence) {
+		console.log('called: '+sequence);
+		callback(sequence, 'bar');
+	},
+	function(callback, sequence) {
+		console.log('called: '+sequence);
+		callback(sequence, 'barbaz');
+	}
+);
